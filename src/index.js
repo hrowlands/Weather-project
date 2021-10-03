4eadfb4d8787a4e352fe625fe9f748a2cae3c682
hrowlands <hannahjrowlands@gmail.com>
2021-10-03 19:35:49 +0100

    Week 7 updates
    added icons, description and wind speed.
    
    also temperature unit conversion.

--- src/index.js
+++ src/index.js
@@ -39,16 +39,49 @@ function showCityName(event) {
 }
 
 function showTemperature(response) {
-  //round this to 0 decimal places
-  let temperature = Math.round(response.data.main.temp);
-  let humidity = response.data.main.humidity;
-
   let temperatureSelector = document.querySelector("#temperature");
   let humiditySelector = document.querySelector("#humidity");
+  let windSpeedSelector = document.querySelector("#windspeed");
+  let descriptionSelector = document.querySelector("#description");
+  let iconSelector = document.querySelector("#icon");
 
-  temperatureSelector.innerHTML = temperature + "°c";
+  //round this to 0 decimal places
+  celsiusTemp = response.data.main.temp;
+  let humidity = response.data.main.humidity;
+  let windspeed = response.data.wind.speed;
+  let description = response.data.weather[0].description;
+
+  temperatureSelector.innerHTML = Math.round(celsiusTemp) + "°C";
   humiditySelector.innerHTML = humidity + "%";
+  windSpeedSelector.innerHTML = windspeed + " km/h";
+  descriptionSelector.innerHTML = description;
+  iconSelector.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let searchForm = document.querySelector("#searchForm");
 searchForm.addEventListener("submit", showCityName);
+
+//temp unit conversion
+function showFahrenheit(event) {
+  event.preventDefault();
+  let temperatureSelector = document.querySelector("#temperature");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  temperatureSelector.innerHTML = Math.round(fahrenheitTemp) + "°F";
+}
+
+function showCelsius(event) {
+  event.preventDefault();
+  let temperatureSelector = document.querySelector("#temperature");
+  temperatureSelector.innerHTML = Math.round(celsiusTemp) + "°C";
+}
+
+let celsiusTemp = null;
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", showFahrenheit);
+
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", showCelsius);
